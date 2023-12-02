0bc1821569ab94de9b3077534ab6bcdd9d322856
dLogan807 <dylanlogan316@gmail.com>
2023-12-02 23:34:21 +1300

    remove about page images

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,17 +1,10 @@
-import { Box, Paper, Text } from "@mantine/core";
-import Image from "next/image";
+import { Box, Button, Paper, Text } from "@mantine/core";
 import classes from "./page.module.css";
-import ewrb from "../assets/ewrb.webp";
-import bulb from "../assets/bulb.webp";
+import Link from "next/link";
 
 export default function AboutUs() {
   return (
     <Box className={classes.about_grid}>
-      <Paper shadow="sm" radius="md" withBorder p="xl" className={classes.ewrb}>
-        <a href="https://kete.mbie.govt.nz/EW/EWPRSearch/practitioner/?id=efe7cde3-b142-df11-917a-005056ae567f">
-          <Image src={ewrb} alt="Electrical Workers Registration Board" />
-        </a>
-      </Paper>
       <Paper
         shadow="sm"
         radius="md"
@@ -23,6 +16,14 @@ export default function AboutUs() {
           I founded Kevin Logan Electrical in 1992 and have since been proudly
           serving the North Shore community. Based in Torbay, you can count on
           me as your local electrician.
+          <br />
+          <br />
+          <Button
+            component={Link}
+            href="https://kete.mbie.govt.nz/EW/EWPRSearch/practitioner/?id=efe7cde3-b142-df11-917a-005056ae567f"
+          >
+            Registered Electrician
+          </Button>
         </Text>
       </Paper>
       <Paper
@@ -43,9 +44,6 @@ export default function AboutUs() {
           At Kevin Logan Electrical, my goal is to provide you with courteous,
           expedient, professional service of the highest calibre.
         </Text>
-      </Paper>
-      <Paper className={classes.bulb_container}>
-        <Image src={bulb} alt="A blue lightbulb" unoptimized />
       </Paper>
     </Box>
   );
