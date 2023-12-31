c69c942a9f97d83f8f0b1ef5fdd812d36eea8ffa
dLogan807 <dylanlogan316@gmail.com>
2023-12-31 15:34:32 +1300

    add security headers
    currently breaks functionality of inline scripts

--- next.config.js
+++ next.config.js
@@ -2,12 +2,44 @@
 
 module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
+  // Policies:
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; font-src 'self'; frame-src https://www.google.com; img-src 'self'; script-src 'self' https://www.google.com/recaptcha/api.js https://www.gstatic.com/; style-src 'self'; manifest-src 'self';",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
         source: "/index.php",
         destination: "/",
-        permanent: true, // triggers 308
+        permanent: true, // Triggers 308
       },
       {
         source: "/aboutus.php",
