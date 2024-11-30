26788c6ff656c6f30c67f6c4be665ccd8ce4cb40
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2024-11-30 13:50:40 +1300

    add map embed and improve not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,4 @@
-import { Button, Paper } from "@mantine/core";
+import { Button, Paper, Text, Box } from "@mantine/core";
 import Link from "next/link";
 
 import classes from "@/not-found.module.css";
@@ -7,16 +7,18 @@ export default function NotFound() {
   const mainSection: string = "main_section";
 
   return (
-    <Paper className={[classes.container, mainSection].join(" ")} withBorder>
-      <h1>404</h1>
-      <h2>Page Not Found</h2>
-      <p>
-        Either the page you&apos;re looking for does not exist or has been
-        moved.
-      </p>
-      <Link href="/">
-        <Button>Go to Kevin Logan Electrical Home</Button>
-      </Link>
-    </Paper>
+    <Box className={[classes.notfound_grid, "content_grid"].join(" ")}>
+      <Paper className={[classes.container, mainSection].join(" ")} withBorder>
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <Text className={classes.explanation}>
+          Either the page you&apos;re looking for does not exist or has been
+          moved.
+        </Text>
+        <Link href="/" className={classes.home_button}>
+          <Button>Go to Kevin Logan Electrical Home</Button>
+        </Link>
+      </Paper>
+    </Box>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <main></main>;
+import { Box, Button, Paper, Stack, Text } from "@mantine/core";
+import Link from "next/link";
+import { Metadata } from "next";
+import classes from "./page.module.css";
+import React from "react";
+import { headers } from "next/headers";
+
+export const metadata: Metadata = {
+  title: "Home | Kevin Logan Electrical - Your Trusted Electrician",
+  description:
+    "Kevin Logan Electrical — providing the North Shore with a quality electrical service for over 30 years.",
+};
+
+export default async function Home() {
+  const mainSection: string = "main_section";
+  const nonce: string = await headers()
+    .then((headers) => headers.get("x-nonce"))
+    .then((rawNonce) => {
+      return rawNonce == undefined ? "" : rawNonce;
+    });
+
+  return (
+    <Box className={[classes.home_grid, "content_grid"].join(" ")}>
+      <iframe
+        nonce={nonce}
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3199.0824953009924!2d174.75661361526056!3d-36.696555478713236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d3b0bb3a49f7d%3A0x91ebdb4ffbccde12!2sKevin%20Logan%20Electrical!5e0!3m2!1sen!2snz!4v1641444320071!5m2!1sen!2snz"
+      ></iframe>
+    </Box>
+  );
 }
