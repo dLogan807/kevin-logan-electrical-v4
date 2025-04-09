9b1ba983449127108bde3d617975fc5403b1c88b
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2025-04-10 10:52:47 +1200

    add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Button, Paper, Text, Box } from "@mantine/core";
+import Link from "next/link";
+
+import classes from "@/error.module.css";
+
+export default function NotFound() {
+  const mainSection: string = "main_section";
+
+  return (
+    <Box className={[classes.error_grid, "content_grid"].join(" ")}>
+      <Paper className={[classes.container, mainSection].join(" ")} withBorder>
+        <h1>Error</h1>
+        <h2>An unexpected error occurred.</h2>
+        <Text className={classes.explanation}>
+          Sorry! Please check your internet connection and try again.
+        </Text>
+        <Link href="/" className={classes.home_button}>
+          <Button>Go to Kevin Logan Electrical Home</Button>
+        </Link>
+      </Paper>
+    </Box>
+  );
+}
