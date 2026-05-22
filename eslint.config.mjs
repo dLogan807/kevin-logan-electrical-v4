95df9d18a3dda1f4e322bd2fbe16328f94cb57f1
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2026-05-22 22:40:00 +1200

    migrate to eslint direct linting

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextPlugin from "@next/eslint-plugin-next";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+    },
+    extends: [
+      ...nextCoreWebVitals,
+      ...compat.extends("eslint:recommended"),
+      ...compat.extends("prettier"),
+    ],
+  },
+]);
