54aef8634bc892278ab561e67db62cb1559a3d6e
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2026-05-22 22:50:44 +1200

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,11 @@
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextPlugin from "@next/eslint-plugin-next";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    rules: {
-      ...nextPlugin.configs.recommended.rules,
-    },
-    extends: [
-      ...nextCoreWebVitals,
-      ...compat.extends("eslint:recommended"),
-      ...compat.extends("prettier"),
-    ],
-  },
+  ...nextCoreWebVitals,
+  ...nextTs,
+  prettier,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
